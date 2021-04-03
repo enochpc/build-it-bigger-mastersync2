a38ed4544ce2f2180fb3f714997ce7ce4a201d81
Ian Nielson <mrenochpc@gmail.com>
2021-04-02 18:58:42 -0600

    Initial commit

---
+++ joketeller/src/main/java/com/enochpc/joketeller/JokeTeller.java
@@ -0,0 +1,9 @@
+package com.enochpc.joketeller;
+
+public class JokeTeller {
+    String joke = "What bear is the most condescending? It's a Pan-Duh!";
+
+    public String getJoke(){
+        return joke;
+    }
+}
